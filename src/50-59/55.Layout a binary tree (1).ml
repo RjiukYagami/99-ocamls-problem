a444fd91440ddf75a1ac84370aecee8e33d98445
emil3566 <emil3566@gmail.com>
2014-09-25 21:17:11 +0200

    rewrite this shit once again git add *git --version

--- src/50-59/55.Layout a binary tree (1).ml
+++ src/50-59/55.Layout a binary tree (1).ml
@@ -13,26 +13,37 @@ let example_layout_tree =
                    leaf 'm'),
          Node('u', Node('p', Empty, Node('s', leaf 'q', Empty)), Empty))
 
+(* i guess it's not working O.o *)
 let layout_binary_tree_1 tree =
-   let all = [] in 
    let rec build_nr tree1 nr = 
       match tree1 with 
-         | Empty -> 0
+         | Empty -> [('-',0)]
          | Node(j, x, y) -> 
             let akt = build_nr x nr in
-            List.append all [(j,x)];
-            build_nr y (nr+akt+1);
-             j
+            let (a,b) = List.hd akt in
+            [(j,b+1)] @ akt @ (build_nr y (b+2))
+            
    in 
-   let rec build_all tree1 level=
+   let rec build_all tree1 level all=
      match tree1 with
         | Empty -> E
         | Node(j, x, y) ->
-           N(j,(List.assoc j all),level, (build_all x (level+1)), (build_all y (level+1)))
+           N(j,(List.assoc j all),level, (build_all x (level+1) all), (build_all y (level+1) all) )
    in 
-   build_nr tree 1;
-   build_all tree 1;;
-       
-         
-        
-         
+   build_all tree 1 ( build_nr tree 1 )
+
+(* ver. 2 good one :D *)
+
+let layout_binary_tree_1_2 tree =
+   let nr = ref 1 in
+      let rec build tree1 level =
+         match tree1 with
+            | Empty -> E
+            | Node (j, x, y) -> 
+               let akt = build x (level+1) in
+               let ile = !nr in
+               nr := !nr + 1;
+               N(j, ile, level, akt,(build y (level+1)))
+      in 
+      build tree 1;;
+            
